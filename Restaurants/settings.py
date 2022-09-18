dc2e03067bf899486cf1177af8603cc9744c3c5b
Manish Pingale <manishpingale2005@gmail.com>
2022-09-18 08:13:48 +0530

    login and signup UI added

--- Restaurants/settings.py
+++ Restaurants/settings.py
@@ -13,9 +13,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 from django.contrib.messages import constants as messages
 
+#Changing Error constant
 MESSAGE_TAGS = {
     messages.ERROR:'danger'
 }
+
+#Redirect url
+LOGIN_URL = '/'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
