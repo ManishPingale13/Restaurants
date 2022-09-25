d413379f680cc520bb9d6d27b69a9ff2a6a400e2
Manish Pingale <manishpingale2005@gmail.com>
2022-09-25 15:25:30 +0530

    S3 dev works

--- Restaurants/urls.py
+++ Restaurants/urls.py
@@ -22,4 +22,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('',include("home.urls")),
     path('dashboard',include("dashboard.urls")),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
