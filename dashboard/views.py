03106c7dfb0e129dacb78abd5d5b852b40f40fd6
Manish Pingale <manishpingale2005@gmail.com>
2022-10-02 06:26:04 +0530

    403 added

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
+@login_required
 def dashboard(request):
-    print("In Dashboard")
-    return render(request,"dashboard/dashboard.html")
+    if not request.user.is_superuser:
+        print("In Dashboard")
+        return render(request,"dashboard/dashboard.html")
+    else:
+        return render(request,"403.html")
